fd2d54181a5d0dc5bbb846745f7ca6cd7410e11e
udhos <udhos>
2024-03-09 18:19:00 -0300

    Report memory limit.

--- boilerplate/boilerplate.go
+++ boilerplate/boilerplate.go
@@ -3,7 +3,9 @@ package boilerplate
 
 import (
 	"fmt"
+	"os"
 	"runtime"
+	"runtime/debug"
 )
 
 // FuncPrintf is a helper type for logging function.
@@ -11,11 +13,16 @@ type FuncPrintf func(format string, v ...any)
 
 // Version returns boilerplate version.
 func Version() string {
-	return "1.2.3"
+	return "1.2.4"
 }
 
 // LongVersion returns boilerplate long version.
 func LongVersion(me string) string {
-	return fmt.Sprintf("%s runtime=%s boilerplate=%s GOOS=%s GOARCH=%s GOMAXPROCS=%d",
-		me, runtime.Version(), Version(), runtime.GOOS, runtime.GOARCH, runtime.GOMAXPROCS(0))
+	return fmt.Sprintf("%s runtime=%s boilerplate=%s GOOS=%s GOARCH=%s GOMAXPROCS=%d GOMEMLIMIT='%s' memory_limit=%d",
+		me, runtime.Version(), Version(), runtime.GOOS, runtime.GOARCH, runtime.GOMAXPROCS(0), os.Getenv("GOMEMLIMIT"), debug.SetMemoryLimit(-1))
 }
+
+/*
+SetMemoryLimit returns the previously set memory limit.
+A negative input does not adjust the limit, and allows for retrieval of the currently set memory limit.
+*/
