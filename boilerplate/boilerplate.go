6c4e9f181c6de5aa5d73b0c8a6b2f43c67b3ee43
udhos <udhos>
2024-12-14 20:10:35 -0300

    Bump version.

--- boilerplate/boilerplate.go
+++ boilerplate/boilerplate.go
@@ -13,7 +13,7 @@ type FuncPrintf func(format string, v ...any)
 
 // Version returns boilerplate version.
 func Version() string {
-	return "1.6.0"
+	return "1.6.2"
 }
 
 // LongVersion returns boilerplate long version.
